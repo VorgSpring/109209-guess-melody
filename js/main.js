6d3ba1cdefef5975034a7a52a1cc547a2387d89b
VorgSpring <drmrSP@yandex.ru>
2016-11-18 22:29:14 +0300

    fixed bug with variable declarations

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
 (function () {
 
-  var template = document.querySelector('template');
+  let template = document.querySelector('template');
 
-  var loadTemplate = function (templateName) {
-    var content = template.content ? template.content : template;
+  let loadTemplate = function (templateName) {
+    let content = template.content ? template.content : template;
     return content.querySelector(templateName).cloneNode(true);
   };
 
 
-  var slides = [
+  let slides = [
     loadTemplate('.main--welcome'),
     loadTemplate('.main--level-artist'),
     loadTemplate('.main--level-genre'),
     loadTemplate('.main--result')
   ];
-  var current = -1;
+  let current = -1;
 
-  var select = function (index) {
+  let select = function (index) {
     current = index;
-    var mainElement = document.querySelector('.main');
+    let mainElement = document.querySelector('.main');
     mainElement.parentNode.replaceChild(slides[index], mainElement);
   };
 

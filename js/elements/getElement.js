de617a9e8e9d5c6ead53221c72187a3ae270c5d3
VorgSpring <drmrSP@yandex.ru>
2016-11-25 15:33:30 +0300

    fixed eslint errors

--- js/elements/getElement.js
+++ js/elements/getElement.js
@@ -1,6 +1,5 @@
 export const getElementFromTemplate = (markup) => {
   let element = document.createElement('template');
   element.innerHTML = markup;
-  console.log(element.content.firstChild);
   return element.content.firstChild;
 };

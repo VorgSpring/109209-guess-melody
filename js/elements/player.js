3014ece9a291377a37534b35c787c2802e3659a2
VorgSpring <drmrSP@yandex.ru>
2016-12-26 16:12:30 +0300

    fixed player bug

--- js/elements/player.js
+++ js/elements/player.js
@@ -36,7 +36,7 @@ const destroyPlayer = (element, state) => {
     state.stopAnimation();
   }
 
-  player.src = null;
+  player.src = '';
   button.onclick = null;
   element.innerHTML = '';
   state = null;
